dc38b033b0bc393a5d63a184226f9bf86ba027e2
Mujtaba500 <126389386+Mujtaba500@users.noreply.github.com>
2023-05-26 17:19:38 +0500

    Optimized code and added btns for diff colors

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 let container = document.getElementById('container')
+// let buttonFor8 = document.querySelector('.btn1')
 
-function makeRows(rowNum) {
-    for ( let i = 0; i < rowNum; i++) {
+// buttonFor8.addEventListener('click', grid)
+let color = 'black'
+
+function grid(size) {
+    let squares = container.querySelectorAll('div')
+    squares.forEach(div => div.remove())
+
+function makeRows(size) {
+    for ( let i = 0; i < size; i++) {
         let row = document.createElement('div');
         container.appendChild(row).className = 'gridRow'
     }
 }
-makeRows(16)
-let rows = Array.from(document.querySelectorAll('.gridRow'))
+makeRows(size)
+  let rows = Array.from(document.querySelectorAll('.gridRow'))
+console.log(rows.length)
 
-function makeColumns(colNum) {
+
+function makeColumns(size) {
    for( let j = 0; j < rows.length; j++ ){
-    for ( let i = 0; i < colNum; i++ ){
+    for ( let i = 0; i < size; i++ ){
         let Col = document.createElement('div');
+        Col.addEventListener('mouseover', colorSquare);
         rows[i].appendChild(Col).className = 'gridCol'
     }
 }
 }
-makeColumns(16)
-let columns = Array.from(document.querySelectorAll('.gridCol'))
-console.log(columns)
-columns.forEach(div => {
-    div.addEventListener('mouseover', colorChange);
-    function colorChange() {
-    div.setAttribute('style', 'background-color:black;');
+makeColumns(size)
+// let columns = Array.from(document.querySelectorAll('.gridCol'))
+// console.log(columns)
+// columns.forEach(div => {
+//     div.addEventListener('mouseover', colorChange);
+//     function colorChange() {
+//     div.setAttribute('style', 'background-color:black;');
+// }
+// }
+// )
+
+ }
+
+function colorSquare() {
+   this.style.backgroundColor = color;
+    
 }
+function colorChange(choice) {
+    color = choice;
 }
-)
+
